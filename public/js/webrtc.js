57ee8e523d38e731c35d0283ea6b5a77f1cc8a13
smdoc1997-ship-it <smdoc1997@users.noreply.github.com>
2026-09-09 16:45:19 +0530

    feat: Add TURN relay servers (port 443/80 TCP/TLS) for cross-network mobile data (4G/5G) and Wi-Fi WebRTC NAT traversal

--- public/js/webrtc.js
+++ public/js/webrtc.js
@@ -24,7 +24,26 @@ class WebRTCManager {
     this.iceServers = [
       { urls: 'stun:stun.l.google.com:19302' },
       { urls: 'stun:stun1.l.google.com:19302' },
-      { urls: 'stun:stun2.l.google.com:19302' }
+      { urls: 'stun:stun2.l.google.com:19302' },
+      { urls: 'stun:stun3.l.google.com:19302' },
+      { urls: 'stun:stun4.l.google.com:19302' },
+      { urls: 'stun:stun.relay.metered.ca:80' },
+      { urls: 'stun:global.stun.twilio.com:3478' },
+      {
+        urls: 'turn:global.relay.metered.ca:80',
+        username: 'e0d9b4b0e5bfb7c4e5b7',
+        credential: 'openrelaypassword'
+      },
+      {
+        urls: 'turn:global.relay.metered.ca:443',
+        username: 'e0d9b4b0e5bfb7c4e5b7',
+        credential: 'openrelaypassword'
+      },
+      {
+        urls: 'turn:global.relay.metered.ca:443?transport=tcp',
+        username: 'e0d9b4b0e5bfb7c4e5b7',
+        credential: 'openrelaypassword'
+      }
     ];
 
     if (this.socket) {
